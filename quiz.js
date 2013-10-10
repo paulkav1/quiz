bbb28fbe8e5b1f4b64acebd4e97183c794b6982e
paulkav1@gmail.com <paulkav1@gmail.com>
2013-10-09 20:25:09 -0400

    to do

--- quiz.js
+++ quiz.js
@@ -1,7 +1,12 @@
-//problems: 
-// 1. I have two programs here using globals to communicate. How to fix?
-// 2. eval
-// 3. feels "hacky"
+//This app allows the user to take a multiple choice quiz on content of your choosing. It should meet the following requirements:
+//
+//Lead the user through a set of questions. The user should only see one question at a time, with new answers presented only after they have answered the current one. You should use jQuery to move between questions as the user enters their response
+//Allow the user to input his or her response to each questions with radio buttons for the user to enter their response
+//When the user answers the last question, the quiz should show his or her overall score
+//Each question should be stored as a JavaScript object, and you’ll want to store your list of questions in an array. For example: var questions = [{question: "What is the capital of the US", choices: ["Washington DC", "NYC", "Silicon Valley"], correct:0}];
+//Let the user know where in the quiz they are at each step (i.e. “Question 3 of 5) 
+//Let the user know their score so far 
+//Let the user know if their previous response was correct 
 
 function formCheck(){
 	item = $('#itemx').val();
