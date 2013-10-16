cc3c3529b0680b3a23a177bfbf18dce62bcff5b8
paulkav1@gmail.com <paulkav1@gmail.com>
2013-10-16 09:10:58 -0400

    Javascript

--- quiz.js
+++ quiz.js
@@ -1,53 +1,46 @@
-//This app allows the user to take a multiple choice quiz on content of your choosing. It should meet the following requirements:
-//
-//Lead the user through a set of questions. The user should only see one question at a time, with new answers presented only after they have answered the current one. You should use jQuery to move between questions as the user enters their response
-//Allow the user to input his or her response to each questions with radio buttons for the user to enter their response
+//This app allows the user to take a multiple choice quiz on content of your choosing.
+//Lead the user through a set of questions, one at a time, with new answers presented only after they have answered the current one. 
+// You should use jQuery to move between questions as the user enters their response
+//Allow the user to input his or her response to each questions with radio buttons
 //When the user answers the last question, the quiz should show his or her overall score
-//Each question should be stored as a JavaScript object, and you’ll want to store your list of questions in an array. For example: var questions = [{question: "What is the capital of the US", choices: ["Washington DC", "NYC", "Silicon Valley"], correct:0}];
-//Let the user know where in the quiz they are at each step (i.e. “Question 3 of 5) 
-//Let the user know their score so far 
-//Let the user know if their previous response was correct 
-
-function formCheck(){
-	item = $('#itemx').val();
-
-	if (item == null || item == ""){			
-	}else{							//item is OK so add to list
-		item = $('#itemx').val();
-		item_list[item_ix] = item	
-		$('#itemx').val('');
-		$('#ct').append('<div><input class="item" type="text" value="' + item + '"><input id="c' + item_ix + '" class="chk" type="checkbox"></div>');
-		item_ix++;	
-		$('#itemx').css("background-color", "white");
-		$('#add').css("color", "black");
-		$('#items').html('<p>' + item_ix + ' items</p>');							
-	}
-
-	var chk = $("input:checked").length;
-	$('#checked').html('<p>' + chk + ' checked</p>');
-	return false;
-};
+//Let the user know:
+// where in the quiz they are at each step (i.e. “Question 3 of 5) 
+// their score so far 
+// if their previous response was correct 
 
 $(document).ready(function(){
-	item_ix = 0;
-	item_list = [];
+	var title = "US Geography";
+	var questions = [
+	{question: "What is the capital of the US?", choices: ["Washington DC", "NYC", "Silicon Valley", "Kansas City", "London"], correct:0},
+	{question: "What is the biggest state in the US?", choices: ["Texas", "Florida", "California", "Illinois", "New York"], correct:0}	
+	];
 
-	$('#rem').click(function(){			// Remove checked items. Make new array and then re-create on screen
-		for (i = item_list.length - 1; i >= 0; i--){  // do array in reverse to preserve numbering
-			var id = eval("$('#c" + i + "')")
-			if (id.is(':checked')){
-				item_list.splice(i, 1)
-			}
-		}
-		$('#ct').empty();
-		for (i = 0; i < item_list.length; i++){
-			$('#ct').append('<div><input class="item" type="text" value="' + item_list[i] + '"><input id="c' + i + '" class="chk" type="checkbox"></div>');
-		}
-		item_ix = item_list.length
-		$('#items').html('<p>' + i + ' items</p>');
-		$('#checked').html('<p>' + 0 + ' checked</p>');				
-	});
+	$('.title').html(title);
+    $('#s0').html(questions[0].question);
+    $('#s1').html(questions[0].choices[0]);
+    $('#s2').html(questions[0].choices[1]);
+    $('#s3').html(questions[0].choices[2]);
+    $('#s4').html(questions[0].choices[3]);  
+    $('#s5').html(questions[0].choices[4]);
 
-	$('.itemx').change(function(){ // not needed, for now anyway
-	});
+	$(".answer").change(function(){
+		if ($("#r1").is(":checked")){
+            alert('r1');
+        }
+        else if ($("#r2").is(":checked")){
+            alert('r2');
+        }
+        else if ($("#r3").is(":checked")){
+            alert('r3');
+        }
+        else if ($("#r4").is(":checked")){
+            alert('r4');
+        }
+        else if ($("#r5").is(":checked")){
+            alert('r5');
+        }                 
+        else{
+        	alert('other');
+        }
+    });
 })
